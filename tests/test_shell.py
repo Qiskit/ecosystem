07d6650858b4a604b7e22d4861f7f7472008ebbb
Iskandar Sitdikov <iskandar.sitdikov@gmail.com>
2021-08-17 14:05:08 -0400

    CI: simple tests for repo

---
+++ tests/test_shell.py
@@ -0,0 +1,33 @@
+"""Tests for shell commands."""
+import os
+import shutil
+from unittest import TestCase
+
+from ecosystem.shell import _execute_command, _clone_repo
+from ecosystem.entities import CommandExecutionSummary
+
+
+class TestShell(TestCase):
+    """Tests shell commands."""
+    def setUp(self) -> None:
+        self.path = "./resources"
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+
+    def tearDown(self) -> None:
+        if os.path.exists(self.path):
+            shutil.rmtree(self.path)
+
+    def test_execute_command(self):
+        """Tests command execution."""
+        execution_result = _execute_command(["ls", "-al"],
+                                            cwd=os.path.dirname(os.path.abspath(__file__)))
+        self.assertTrue(isinstance(execution_result, CommandExecutionSummary))
+        self.assertTrue(len(execution_result.logs) > 0)
+
+    def test_clone_repo(self):
+        """Tests repo cloning."""
+        clone_res = _clone_repo("https://github.com/octocat/Hello-World",
+                                directory=self.path)
+        self.assertTrue(os.path.exists(f"{self.path}/Hello-World"))
+        self.assertEqual(clone_res.code, 0)
