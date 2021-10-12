333b350447861d58c84c95146b4da11112028542
Mica <mika89700@gmail.com>
2021-10-12 20:25:17 +0200

    black

--- tests/manager.py
+++ tests/manager.py
@@ -12,9 +12,7 @@ class TestManager(TestCase):
 
     def setUp(self) -> None:
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        with open(
-            "{}/resources/issue.md".format(current_dir), "r"
-        ) as issue_body_file:
+        with open("{}/resources/issue.md".format(current_dir), "r") as issue_body_file:
             self.issue_body = issue_body_file.read()
 
     def test_issue_parsing_cli(self):
@@ -27,10 +25,26 @@ class TestManager(TestCase):
         output_value = capturedOutput.getvalue().split("\n")
 
         self.assertEqual(output_value[0], "::set-output name=SUBMISSION_NAME::awesome")
-        self.assertEqual(output_value[1], "::set-output name=SUBMISSION_REPO::http://github.com/awesome/awesome")
-        self.assertEqual(output_value[2], "::set-output name=SUBMISSION_DESCRIPTION::An awesome repo for awesome project")
-        self.assertEqual(output_value[3], "::set-output name=SUBMISSION_LICENCE::Apache License 2.0")
-        self.assertEqual(output_value[4], "::set-output name=SUBMISSION_CONTACT::toto@gege.com")
-        self.assertEqual(output_value[5], "::set-output name=SUBMISSION_ALTERNATIVES::tititata")
-        self.assertEqual(output_value[6], "::set-output name=SUBMISSION_AFFILIATIONS::_No response_")
-        self.assertEqual(output_value[7], "::set-output name=SUBMISSION_LABELS::['tool', 'tutorial']")
+        self.assertEqual(
+            output_value[1],
+            "::set-output name=SUBMISSION_REPO::http://github.com/awesome/awesome",
+        )
+        self.assertEqual(
+            output_value[2],
+            "::set-output name=SUBMISSION_DESCRIPTION::An awesome repo for awesome project",
+        )
+        self.assertEqual(
+            output_value[3], "::set-output name=SUBMISSION_LICENCE::Apache License 2.0"
+        )
+        self.assertEqual(
+            output_value[4], "::set-output name=SUBMISSION_CONTACT::toto@gege.com"
+        )
+        self.assertEqual(
+            output_value[5], "::set-output name=SUBMISSION_ALTERNATIVES::tititata"
+        )
+        self.assertEqual(
+            output_value[6], "::set-output name=SUBMISSION_AFFILIATIONS::_No response_"
+        )
+        self.assertEqual(
+            output_value[7], "::set-output name=SUBMISSION_LABELS::['tool', 'tutorial']"
+        )
