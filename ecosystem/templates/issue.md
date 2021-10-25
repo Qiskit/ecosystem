1ef05d57e76392443fa5b62248f2f1db0e5a7083
Michael Rollin <mrollin.ext@orange.com>
2021-10-25 14:18:08 +0200

    used ttp

---
+++ ecosystem/templates/issue.md
@@ -0,0 +1,27 @@
+### Github repo
+
+{{ repo_url }}
+
+### Description
+
+{{ description }}
+
+### Email
+
+{{ email }}
+
+### Alternatives
+
+{{ alternatives }}
+
+### License
+
+{{ license }}
+
+### Affiliations
+
+{{ affiliations }}
+
+### Tags
+
+{{ tags }}
--- ecosystem/utils/submission_parser.py
+++ ecosystem/utils/submission_parser.py
@@ -1,5 +1,7 @@
 """Parser for issue submission."""
 import re
+from ttp import ttp
+import json
 
 from ecosystem.models.repository import Repository
 
@@ -7,25 +9,35 @@ from ecosystem.models.repository import Repository
 def parse_submission_issue(body_of_issue: str) -> Repository:
     """Parse issue body."""
 
-    parse = re.findall(r"^([\s\S]*?)(?:\n{2,}|\Z)", body_of_issue, re.M)
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    with open(
+        "{}/../templates/issue.md".format(current_dir), "r"
+    ) as issue_body_tpl:
+        issue_template = issue_body_tpl.read()
 
-    repo_name = parse[1].split("/")
+    parser = ttp(data=body_of_issue, template=issue_template)
+    parser.parse()
+    parser.result(format='json')[0]
+    
+    parse_json = json.loads(result)
+
+    repo_name = parse_json[0]['repo_url'].split("/")
 
     name = repo_name[-1]
-    url = parse[1]
-    description = parse[3]
-    contact_info = parse[5]
-    alternatives = parse[7]
-    licence = parse[9]
-    affiliations = parse[11]
+    url = parse_json[0]['repo_url']
+    description = parse_json[0]['description']
+    contact_info = parse_json[0]['email']
+    alternatives = parse_json[0]['alternatives']
+    license = parse_json[0]['license']
+    affiliations = parse_json[0]['affiliations']
 
-    labels = re.findall(r"([\w\]+)([\w\-\_]+)", parse[13])
+    labels = re.findall(r"([\w\]+)([\w\-\_]+)", parse_json[0]['tags'])
 
     return Repository(
         name,
         url,
         description,
-        licence,
+        license,
         contact_info,
         alternatives,
         affiliations,
