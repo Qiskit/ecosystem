a3eaab3ac9dea9c366f64a64c88e2f95007cb09f
Michaël <mika89700@gmail.com>
2021-10-26 15:45:13 +0200

    Bug submission ci (#61)
    * Update configuration.py
    
    * Update action.yml
    
    * Update submission.yml
    
    * Update runner.py
    
    * Update submission.yml
    
    * Update submission.yml
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update runner.py
    
    * black
    
    * Update execution_summary.py
    
    * Update execution_summary.py
    
    * Update runner.py
    
    * Update runner.py
    
    * Update action.yml
    
    * Update submission.yml
    
    * Update runner.py
    
    * Update execution_summary.py
    
    * black
    
    * Update requirements.txt
    
    * Update requirements.txt
    
    * Update submission_parser.py
    
    * Update submission_parser.py
    
    * Update submission_parser.py
    
    * black
    
    * Fix formating for logs
    
    Co-authored-by: Iskandar Sitdikov <IceKhan13@users.noreply.github.com>

--- ecosystem/runners/runner.py
+++ ecosystem/runners/runner.py
@@ -15,6 +15,7 @@ from ecosystem.models import (
 from ecosystem.models.repository import Repository
 from ecosystem.utils import QiskitEcosystemException
 from ecosystem.utils import logger as ecosystem_logger
+from ecosystem.utils.utils import set_actions_output
 
 
 class Runner:
@@ -131,8 +132,17 @@ class Runner:
 
         try:
             result = self.workload()
+            _, executive_summary = result
+            logs_error = []
+            logs_fail = []
+            for element in executive_summary:
+                logs_error += element.get_error_logs()
+                logs_fail += element.get_fail_logs()
+            set_actions_output([("ERROR", logs_error)])
+            set_actions_output([("FAIL", logs_fail)])
+
         except Exception as exception:  # pylint: disable=broad-except
-            result = ("-", CommandExecutionSummary(1, [], summary=str(exception)))
+            result = ("-", [CommandExecutionSummary(1, [], summary=str(exception))])
             self.logger.error(exception)
         self.tear_down()
         return result
--- ecosystem/utils/submission_parser.py
+++ ecosystem/utils/submission_parser.py
@@ -1,5 +1,6 @@
 """Parser for issue submission."""
 import re
+import mdformat
 
 from ecosystem.models.repository import Repository
 
@@ -7,7 +8,9 @@ from ecosystem.models.repository import Repository
 def parse_submission_issue(body_of_issue: str) -> Repository:
     """Parse issue body."""
 
-    parse = re.findall(r"^([\s\S]*?)(?:\n{2,}|\Z)", body_of_issue, re.M)
+    issue_formatted = mdformat.text(body_of_issue)
+
+    parse = re.findall(r"^([\s\S]*?)(?:\n{2,}|\Z)", issue_formatted, re.M)
 
     repo_name = parse[1].split("/")
 
