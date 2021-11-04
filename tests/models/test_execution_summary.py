17aaa0bfe4884d8e10998f366bf3104e754b9608
Michaël <mika89700@gmail.com>
2021-11-04 15:19:34 +0100

    Update test_execution_summary.py

--- tests/models/test_execution_summary.py
+++ tests/models/test_execution_summary.py
@@ -24,6 +24,22 @@ class TestExecutionSummary(TestCase):
         )
 
         self.assertTrue(summary_with_qiskit_deprecation.has_qiskit_deprecation_logs)
+        
+        aqua_log_message = (
+            "/srv/conda/envs/notebook/"
+            "lib/python3.8/site-packages/qiskit/aqua/__init__.py:86"
+            ": DeprecationWarning: The package qiskit.aqua is deprecated."
+            " It was moved/refactored to qiskit-terra For more"
+            " information see <https://github.com/Qiskit"
+            "/qiskit-aqua/blob/main/README.md#migration-guide>"
+            " warn_package('aqua', 'qiskit-terra')"
+        )
+
+        summary_with_aqua_deprecation = CommandExecutionSummary(
+            code=0, logs=[aqua_log_message]
+        )
+        
+        self.assertFalse(summary_with_aqua_deprecation.has_qiskit_deprecation_logs)
 
         clear_summary = CommandExecutionSummary(code=0, logs=[])
 
