57b3b8cd158ed08f3ceb42df58f95deac32a5b77
Michaël <mika89700@gmail.com>
2021-11-04 14:50:04 +0100

    Update runner.py

--- ecosystem/runners/runner.py
+++ ecosystem/runners/runner.py
@@ -141,8 +141,10 @@ class Runner:
             logs_error = []
             logs_fail = []
             for element in executive_summary:
+                logs_depreciation += element.get_qiskit_depreciation_logs()
                 logs_error += element.get_error_logs()
                 logs_fail += element.get_fail_logs()
+            set_actions_output([("DEPRECIATION", "\n".join(logs_depreciation))])
             set_actions_output([("ERROR", "\n".join(logs_error))])
             set_actions_output([("FAIL", "\n".join(logs_fail))])
 
