f8bebc0ef36c952097e4d219190824c908eebeec
Michaël <mika89700@gmail.com>
2021-11-05 10:18:21 +0100

    Update manager.py

--- ecosystem/manager.py
+++ ecosystem/manager.py
@@ -59,7 +59,6 @@ class Manager:
             json={
                 "event_type": "check_project",
                 "client_payload": {
-                    "repo": repo,
                     "repo_url": repo_url,
                     "issue_id": issue_id,
                     "branch_name": branch_name,
