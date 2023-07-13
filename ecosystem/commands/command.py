643621f3e2a1eb2a59a2f25f58e5f6a840a948f1
Michaël <michael.rollin@protonmail.com>
2023-07-13 13:47:24 +0200

    test with branch qiskit-ecosystem-test

--- ecosystem/commands/command.py
+++ ecosystem/commands/command.py
@@ -94,7 +94,7 @@ class CloneRepoCommand(Command):
         Return: CommandExecutionSummary
         """
         return cls.subprocess_execute(
-            ["git", "-C", directory, "clone", repo], name="Clone repo"
+            ["git", "-C", directory, "--branch", "qiskit-ecosystem-test","clone", repo], name="Clone repo"
         )
 
 
