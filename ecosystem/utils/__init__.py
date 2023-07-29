ad90b34a7a0960696450d1a1ec1391f79ed468ce
Michaël <mika89700@gmail.com>
2023-07-29 13:35:13 +0200

    [BUG] Unable to output (#460)
    * force string for output
    
    * Revert "force string for output"
    
    This reverts commit cabecb9758d485839bdcb00afd09bb121e6ce4a6.
    
    * force string output
    
    * use with print
    
    * convert to single line
    
    * not join \n
    
    * 2 depreciation I forget last time
    
    * add assert for set_actions_output
    Co-authored-by: Frank Harkins <frankharkins@hotmail.co.uk>
    
    * comment changes

--- ecosystem/utils/__init__.py
+++ ecosystem/utils/__init__.py
@@ -1,5 +1,6 @@
 """Utility functions and classes."""
 
 from .utils import logger
+from .utils import set_actions_output
 from .utils import OneLineExceptionFormatter
 from .submission_parser import parse_submission_issue
