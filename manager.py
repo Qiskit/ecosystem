bec5ea94585b016a389f4f172f7dc1e65be6d1ff
minwook <minwook0106@gmail.com>
2023-09-07 13:03:41 +0900

    fix typo for test function in manager.py

--- manager.py
+++ manager.py
@@ -4,17 +4,17 @@ Available commands:
 
 1. Run tests within repository.
 ```shell
-python manager.py standard_tests https://github.com/<ACCOUNT>/<REPOSITORY_NAME> --tox_python=<py36,py37,py38,py39>
+python manager.py python_standard_tests https://github.com/<ACCOUNT>/<REPOSITORY_NAME> --tox_python=<py36,py37,py38,py39>
 ```
 
 2. Run tests against stable version of Qiskit.
 ```shell
-python manager.py stable_compatibility_tests https://github.com/<ACCOUNT>/<REPOSITORY_NAME> --tox_python=<py36,py37,py38,py39>
+python manager.py python_stable_tests https://github.com/<ACCOUNT>/<REPOSITORY_NAME> --tox_python=<py36,py37,py38,py39>
 ```
 
 3. Run tests against dev version of Qiskit.
 ```shell
-python manager.py dev_compatibility_tests https://github.com/<ACCOUNT>/<REPOSITORY_NAME> --tox_python=<py36,py37,py38,py39>
+python manager.py python_dev_tests https://github.com/<ACCOUNT>/<REPOSITORY_NAME> --tox_python=<py36,py37,py38,py39>
 ```
 
 4. Get parse issue.
