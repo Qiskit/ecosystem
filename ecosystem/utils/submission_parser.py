4e298ffa65d64aaedafca62315f63ec7ac7ea378
Frank Harkins <frankharkins@hotmail.co.uk>
2024-05-01 18:52:58 +0100

    Fix submission process (#701)
    ### Summary
    
    The submission process was broken as some entries in the issue template
    were being ignored by the parser. This PR fixes that and makes the
    submission process more robust so similar issues don't occur in the
    future.
    
    ### Details
    
    Currently, to add a new field to the issue template, you need to edit
    four different files to make sure the field makes it through to the
    database. This PR avoids that by explicitly mapping field IDs in the
    issue template to attributes of the repository model and writing the
    entry immediately. After this change, you only need to edit the issue
    template and the repository model.
    
    I've also added a test check the fields in the issue template match
    fields in the repository model.
    
    ---------
    
    Co-authored-by: Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>

--- ecosystem/utils/submission_parser.py
+++ ecosystem/utils/submission_parser.py
@@ -1,23 +1,63 @@
 """Parser for issue submission."""
-from collections import defaultdict
+
+from pathlib import Path
 import mdformat
+import yaml
 
 from ecosystem.models.repository import Repository
 
 
-def _clean_section(section: str) -> {str: str}:
-    """For a section, return a tuple with a title and "clean section".
-    A clean section is without new lines and strip spaces"""
-    paragraphs = section.split("\n")
-    section = (" ").join(
-        [paragraph.strip() for paragraph in paragraphs[1:] if paragraph]
+def _parse_section(section: str, label_to_id: dict[str, str]) -> tuple[str, str]:
+    """For a section, return its field ID and the content.
+    The content has no newlines and has spaces stripped.
+    """
+    lines = section.split("\n")
+    content = " ".join(line.strip() for line in lines[1:] if line)
+    label = lines[0].strip()
+    field_id = label_to_id[label]
+    return field_id, content
+
+
+def _get_label_to_id_map() -> dict[str, str]:
+    """Create a dict that maps a fields "label" to its `id` from the issue
+    template
+    """
+    issue_template = yaml.load(
+        Path(".github/ISSUE_TEMPLATE/submission.yml").read_text(),
+        Loader=yaml.SafeLoader,
     )
-    title = paragraphs[0].strip()
-    return (title, section)
+    label_to_id = {
+        form["attributes"]["label"]: form["id"]
+        for form in issue_template["body"]
+        if form["type"] != "markdown"
+    }
+    return label_to_id
 
 
 def parse_submission_issue(body_of_issue: str) -> Repository:
     """Parse issue body.
+
+    The GitHub issue is a collection of "fields", each of which has a
+    "label" and an "ID" specified in the template. We require IDs in the
+    template to match argument names of the Repository model.
+
+    We recieve issues as a markdown string. The markdown contains a section for
+    each field; the heading of a section is the "label", and the content that
+    follows it is the information the user submitted for that field.
+
+    ```
+    ### Field label
+
+    Content user has submitted for that field.
+    ```
+
+    This function parses the markdown to create a dict of { label: content },
+    then, using the issue template, transforms labels to IDs to create a
+    dictionary { id: content }. Since the IDs match arguments of the Repository
+    constructor, this dict is the "args" needed to create the Repository object.
+
+    Since users can only submit strings, we map the string "_No response_" to
+    None and parse the "labels" field into a list.
 
     Args:
         body_of_issue: body of an GitHub issue in markdown
@@ -27,35 +67,18 @@ def parse_submission_issue(body_of_issue: str) -> Repository:
 
     issue_formatted = mdformat.text(body_of_issue)
 
-    sections = defaultdict(
-        None, [_clean_section(s) for s in issue_formatted.split("### ")[1:]]
-    )
+    md_sections = issue_formatted.split("### ")[1:]
+    label_to_id = _get_label_to_id_map()
+    args = dict(_parse_section(s, label_to_id) for s in md_sections)
 
-    repo_name = sections["Github repo"].split("/")[-1]
+    args = {
+        field_id: (None if content == "_No response_" else content)
+        for field_id, content in args.items()
+    }
 
-    name = repo_name
-    url = sections["Github repo"]
-    description = sections["Description"]
-    contact_info = sections["Email"]
-    alternatives = sections["Alternatives"]
-    licence = sections["License"]
-    affiliations = sections["Affiliations"]
-    website = sections["Website"]
-    if website == "_No response_":
-        website = None
+    if args["labels"] is None:
+        args["labels"] = []
+    else:
+        args["labels"] = [x.strip() for x in args["labels"].split(",")]
 
-    labels = [l.strip() for l in sections["Labels"].split(",")]
-    if labels == ["_No response_"]:
-        labels = []
-
-    return Repository(
-        name=name,
-        url=url,
-        description=description,
-        licence=licence,
-        contact_info=contact_info,
-        alternatives=alternatives,
-        affiliations=affiliations,
-        labels=labels,
-        website=website,
-    )
+    return Repository(**args)
