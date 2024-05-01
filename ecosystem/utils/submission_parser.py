155fa763e022d0047fd65202f9deec38cffa0740
Frank Harkins <frankharkins@hotmail.co.uk>
2024-05-01 18:23:52 +0100

    Don't store mapping in function attribute

--- ecosystem/utils/submission_parser.py
+++ ecosystem/utils/submission_parser.py
@@ -7,31 +7,31 @@ import yaml
 from ecosystem.models.repository import Repository
 
 
-def _parse_section(section: str) -> tuple[str, str]:
+def _parse_section(section: str, label_to_id: dict) -> tuple[str, str]:
     """For a section, return its field ID and the content.
     The content has no newlines and has spaces stripped.
     """
     lines = section.split("\n")
     content = " ".join(line.strip() for line in lines[1:] if line)
     label = lines[0].strip()
-    field_id = _label_to_id(label)
+    field_id = label_to_id[label]
     return field_id, content
 
 
-def _label_to_id(label: str) -> str:
-    """Given a fields "label", find its `id` from the issue template"""
-    if not hasattr(_label_to_id, "map"):
-        # Store as attribute so we only need to read the template once
-        issue_template = yaml.load(
-            Path(".github/ISSUE_TEMPLATE/submission.yml").read_text(),
-            Loader=yaml.SafeLoader,
-        )
-        _label_to_id.map = {
-            form["attributes"]["label"]: form["id"]
-            for form in issue_template["body"]
-            if form["type"] != "markdown"
-        }
-    return _label_to_id.map[label]
+def _get_label_to_id_map() -> dict[str, str]:
+    """Create a dict that maps a fields "label" to its `id` from the issue
+    template
+    """
+    issue_template = yaml.load(
+        Path(".github/ISSUE_TEMPLATE/submission.yml").read_text(),
+        Loader=yaml.SafeLoader,
+    )
+    label_to_id = {
+        form["attributes"]["label"]: form["id"]
+        for form in issue_template["body"]
+        if form["type"] != "markdown"
+    }
+    return label_to_id
 
 
 def parse_submission_issue(body_of_issue: str) -> Repository:
@@ -68,7 +68,8 @@ def parse_submission_issue(body_of_issue: str) -> Repository:
     issue_formatted = mdformat.text(body_of_issue)
 
     md_sections = issue_formatted.split("### ")[1:]
-    args = dict(_parse_section(s) for s in md_sections)
+    label_to_id = _get_label_to_id_map()
+    args = dict(_parse_section(s, label_to_id) for s in md_sections)
 
     args = {
         field_id: (None if content == "_No response_" else content)
