098a7e97103ec76a6c2fc45306726225dd1c78c9
Luciano Bello <766693+1ucian0@users.noreply.github.com>
2026-04-23 10:55:11 +0200

    new section: badge (#1108)

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -16,11 +16,11 @@ from ecosystem.member import Member
 def get_community_repo() -> Member:
     """Return main mock repo."""
     return Member(
-        name="mock-qiskit-terra",
-        url="https://github.com/MockQiskit/mock-qiskit-wsdt.terra",
-        description="Mock description for repo. wsdt",
+        name="mock-qiskit",
+        url="https://github.com/MockQiskit/mock-qiskit",
+        description="Mock description for repo",
         licence="Apache 2.0",
-        labels=["mock", "tests", "wsdt"],
+        labels=["mock", "tests"],
         badge="https://qisk.it/e",
     )
 
@@ -170,7 +170,7 @@ class TestCli(TestCase):
         self.assertDictEqual(expected, retrieved)
 
     @mock.patch("requests.get", new=mocked_get_request)
-    def test_update_badges(self):
+    def test_create_badge_endpoints(self):
         """Tests creating badges."""
         commu_success = get_community_repo()
         dao = DAO(self.path)
@@ -183,8 +183,8 @@ class TestCli(TestCase):
         cli_members.current_dir = self.path
         cli_members.dao = dao
 
-        # create badges
-        cli_members.update_badges()
+        # create badge endpoints
+        cli_members.create_badge_endpoints()
 
         # gets a short url and updates the list in qisk.it/ecosystem-badges
         cli_members.update_badge_list()
