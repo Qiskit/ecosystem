ba2beab2d9da176a272ec3045c1dc02b9222aade
Luciano Bello <bel@zurich.ibm.com>
2026-07-02 12:17:49 +0200

    black

--- ecosystem/validation/test_license.py
+++ ecosystem/validation/test_license.py
@@ -11,17 +11,22 @@
 # that they have been altered from the originals.
 
 """License validations"""
-import pytest
 
+import pytest
 
 # pylint: disable=missing-function-docstring
 
+
 def test_O01(member):
-    if not member.license and not member.github:  # probably too early, it is a new submission
-        pytest.skip('too early, new submission')
+    if (
+        not member.license and not member.github
+    ):  # probably too early, it is a new submission
+        pytest.skip("too early, new submission")
     if member.license:
         assert len(str(member.license)) > 1
     elif member.github and hasattr(member.github, "license") and member.github.license:
         assert len(str(member.github.license)) > 1
     else:
-        assert False, "No license could be found. Populate 'member.license' as soon as possible."
+        assert (
+            False
+        ), "No license could be found. Populate 'member.license' as soon as possible."
